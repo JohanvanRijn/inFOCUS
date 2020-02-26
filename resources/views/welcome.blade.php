feaae816480d9de0f102e330c82390ae753d9df6
Jaimy horst <97043265@st.deltion.nl>
2020-02-26 14:45:44 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    koekje
+                    broodje frikandel
                 </div>
 
                 <div class="links">
