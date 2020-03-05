8a7d394a29856d0a20e48232f249054da362f79c
mikamul <49638171+mikamul@users.noreply.github.com>
2020-03-05 13:08:18 +0100

    profile styling

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,11 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>TESTJE</h1>
+	<!-- <h1>TESTJE</h1>
 	<table>
 		<tr>
 			<td>{{$user->name}}</td>
 			<td>{{$user->email}}</td>
 		</tr>
-	</table>
-	@endsection
+	</table> -->
+	<div class="container">
+        <div class="bannerContainer">
+            <div class="bannerPic">
+                <div class="bannerText">
+                    <div class="name_review">
+                        <h1>Mika Mul</h1>
+                        <div class="avReview">
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star"></span>
+                            <span class="fa fa-star"></span>
+                        </div>
+                    </div>
+                    <p class="username">mika_JEMJ</p>
+                    <p class="omschrijving">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+            <div class="profilePicContainer">
+                <div class="profilePic"></div>
+            </div>
+        </div>
+        <div class="menu">
+            <a>Posts</a>
+            <a>Recenties</a>
+            <a>Bewerken</a>
+        </div>
+        <div class="content">
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+@endsection
