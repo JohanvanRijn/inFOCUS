66a994987c313621b6de68eeed2a2a8d55f2f248
mikamul <49638171+mikamul@users.noreply.github.com>
2020-03-06 13:08:53 +0100

    profile tabs

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -31,12 +31,14 @@
             </div>
         </div>
         <div class="menu">
-            <a>Posts</a>
-            <a>Recenties</a>
-            <a href={{route('profileEdit')}}>Bewerken</a>
+            <ul class="tabs">
+                <li @if(!isset($_GET['tab']) || $_GET['tab'] == 1) @endif class="active" onclick="openTab(1, this);">Posts</li>
+                <li @if(!isset($_GET['tab']) || $_GET['tab'] == 2) @endif class="active" onclick="openTab(2, this);">Recenties</li>
+                <li @if(!isset($_GET['tab']) || $_GET['tab'] == 3) @endif class="active" onclick="openTab(3, this);">Bewerken</li>
+            </ul>
         </div>
         <div class="content">
-            <div class="posts">
+            <div class="contentX posts" id="tab1">
                 <div class="postContainer">
                     <div class="postpicca postPic1">
                     </div>
@@ -94,9 +96,53 @@
                     </div>
                 </div>
             </div>
+            <div class="contentX recenties" id="tab2">
+                <h1>RECENTIES</h1>
+            </div>
+            <div class="contentX bewerken" id="tab3">
+                <h1>BEWERKEN</h1>
+            </div>
         </div>  
     </div>
     <div class="footer">
 
     </div>
+
+    <script>
+    $(document).ready(function() {
+        $('.contentX').hide();
+        //direct naar tabblad door /tabladnaam/ achter url
+        @if(!isset($_GET['tab'])) $('.contentX:first').show(); @endif
+        @if(isset($_GET['tab'])) openTab("{{ $_GET['tab'] }}", this) @endif
+
+        var url = window.location.pathname;
+        var get = url.substr(url.lastIndexOf('/') + 1);
+
+        switch (get) {
+            case "posts":
+                openTab("1", this);
+                $('.tabs li:nth-child(1)').addClass('active');
+                break;
+            case "recenties":
+                openTab("2", this);
+                $('.tabs li:nth-child(2)').addClass('active');
+                break;
+            case "bewerken":
+                openTab("3", this);
+                $('.tabs li:nth-child(3)').addClass('active');
+                break;
+            default:
+                text = "No value found";
+                console.log(text);
+        }
+    });  
+    function openTab(id, elem) {
+        $('.contentX').hide();
+        if(elem != "false") {
+            $('.tabs li').removeClass('active');
+            $(elem).addClass('active');
+        }
+        $('#tab'+id).show();
+    }
+    </script>
 @endsection
